6eecfa247ff20040fecd01f262fbf31fa28ccf6d
JoeAE750 <106939953+JoeAE750@users.noreply.github.com>
2024-05-31 08:06:52 +0000

    loginbackend2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    # "postgres://sysvitadb_user:NBBRKfOPt4enLTkMZrcrWjTUzsGd5FRO@dpg-cpal71v109ks73aq24s0-a.oregon-postgres.render.com/sysvitadb"
+    # "postgresql://sysvitadb_user:NBBRKfOPt4enLTkMZrcrWjTUzsGd5FRO@dpg-cpal71v109ks73aq24s0-a.oregon-postgres.render.com/sysvitadb"
 
     db.init_app(app)
 
